4e57845d0ec5ff3789ce3377c92d68896b60e5f5
vskumar12017 <shanvem@yahoo.com>
2017-01-29 12:13:30 +0530

    This is sample program written in Java for GIT

---
+++ sample.java
@@ -0,0 +1,7 @@
+// File: Sample.java
+public class Sample {
+  public static void main(String args[]) {
+  System.out.println("Hello, This Java Class!! for learning ....");
+  System.out.println("Hello, This is Selenium class! for learning ....");
+  }
+}
